5481ffe5bb2a19da62ab9ff426a5118d33fe62a3
yousifdabbagh <Yousifdabbagh@gmail.com>
2022-01-30 16:10:54 -0500

    created function to remove whitespace

--- backend/scrape.py
+++ backend/scrape.py
@@ -11,13 +11,16 @@ def file2Soup(fileLocation):
         page = f.read()
     return BeautifulSoup(page, 'html.parser')
 
+def removeUnicodeSpace(list):
+    return [i.replace(u"\xa0",u"").strip() for i in list]
 
 if __name__ == "__main__":
     #soup = url2Soup("https://cg2019.gems.pro/Result/ShowPerson_List.aspx?SetLanguage=en-CA")
     soup = file2Soup("c:\\Users\\Yousif\\Dropbox\\BrockChatbot\\backend\\allAthletes.htm")
     table = soup.find('table',id="ctl00_ContentPlaceHolder1_tblParticipant") 
     athletes_sports = [i.get_text() for i in table.find_all("td")]
-    athletes_sports = [i.replace(u"\xa0",u"") for i in athletes_sports]
+
+    athletes_sports = removeUnicodeSpace(athletes_sports)
     print(athletes_sports)
 
 
