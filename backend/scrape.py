226522d5379ae4ff7ed0342a4e8d2bb681cb068d
yousifdabbagh <Yousifdabbagh@gmail.com>
2022-01-30 16:01:08 -0500

    commented test lines

--- backend/scrape.py
+++ backend/scrape.py
@@ -13,8 +13,8 @@ def file2Soup(fileLocation):
 
 
 if __name__ == "__main__":
-    soup = url2Soup("https://cg2019.gems.pro/Result/ShowPerson_List.aspx?SetLanguage=en-CA")
-    #soup = file2Soup("c:\\Users\\Yousif\\Dropbox\\BrockChatbot\\backend\\allAthletes.htm")
+    #soup = url2Soup("https://cg2019.gems.pro/Result/ShowPerson_List.aspx?SetLanguage=en-CA")
+    soup = file2Soup("c:\\Users\\Yousif\\Dropbox\\BrockChatbot\\backend\\allAthletes.htm")
     table = soup.find('table',id="ctl00_ContentPlaceHolder1_tblParticipant") 
     athletes_sports = [i.get_text() for i in table.find_all("td")]
     athletes_sports = [i.replace(u"\xa0",u"") for i in athletes_sports]
