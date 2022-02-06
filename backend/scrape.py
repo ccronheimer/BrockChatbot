67447c65ccc741b83c28425a8a7756e05a141100
yousifdabbagh <Yousifdabbagh@gmail.com>
2022-02-06 14:45:09 -0500

    Updated DB script to include individual and multiple input functions, including db key generation and key checking

--- backend/scrape.py
+++ backend/scrape.py
@@ -6,51 +6,37 @@ import pathlib
 WORKING_DIRECTORY = str(pathlib.Path().resolve())
 
 
+
+## Returns the soup of a url
 def url2Soup(url):
     page = requests.get(url)
     return BeautifulSoup(page.text, 'html.parser')
 
+## Returns the soup of an html file
 def file2Soup(fileLocation):
     with open(fileLocation, 'r') as f:
         page = f.read()
     return BeautifulSoup(page, 'html.parser')
 
-def file2Soup2(filelocation):
+## Returns the soup of an html file with utf8 encoding
+def file2Soup_utf(filelocation):
     with open(filelocation, encoding='utf8') as f:
         page = f.read()
     return BeautifulSoup(page, 'html.parser')
 
+## Remove all trailing spaces from list items and space unicodes
 def removeUnicodeSpace(list):
     return [i.replace(u"\xa0",u"").strip() for i in list]
 
-def remove_from_back(table, times):
-    for i in range(times):
-        table.pop()
-    return None
-
-def remove_indices(table, begin, end):
-    del table [begin:end]
-    return None
-
+## Returns scraped player+sport data from gemspro 2019 html for testing pruposes
 def scrapeAthleteData_gemspro(filename="allAthletes.htm"):
     soup = file2Soup(WORKING_DIRECTORY+"\\backend\\allAthletes.htm")
-    tables = soup.find_all('table')
-    athlete_and_sports_table = tables[2]
+    table = soup.find('table',id="ctl00_ContentPlaceHolder1_tblParticipant") 
+    athletes_sports = [i.get_text() for i in table.find_all("td")]
+    athletes_sports = removeUnicodeSpace(athletes_sports)
+    athletes_sports = grabEveryN_fromList(athletes_sports,2)
+    return athletes_sports
 
-    sports_table_to_scrap = athlete_and_sports_table.find_all('div', class_=("LM_SportName"))
-    athlete_table_to_scrap = athlete_and_sports_table.find_all('a')
-    sports_table = []
-    athlete_table = []
-
-    for sport in sports_table_to_scrap:
-        sports_table.append(sport.get_text())
-    
-    for athlete in athlete_table_to_scrap:
-        athlete_table.append(athlete.get_text())
-
-    remove_from_back(athlete_table,14)
-    remove_indices(athlete_table,0,7)
-    return athlete_table
 
 def scrapeCompetitors_nigaragames():
     soup = url2Soup("https://niagara2022games.ca/sports/#Athletics")
@@ -59,6 +45,10 @@ def scrapeCompetitors_nigaragames():
     Athletes = removeUnicodeSpace(Athletes)
     return Athletes
 
+def grabEveryN_fromList(list,n):
+    return [list[i*n:i*n+n] for i in range(int(len(list)/n))]
+
+## Return the schedule on niagara games website.
 def scrapeSchedule_niagaragames():
     soup = file2Soup(WORKING_DIRECTORY+"\\backend\\schedule.htm")
     table = soup.find('tbody', id = "ctl00_ctl00_ContentPlaceHolderBasicMaster_ContentPlaceHolder1_secGroup1_1_secGroup1_1_SectionContent") 
@@ -66,16 +56,19 @@ def scrapeSchedule_niagaragames():
     stringSchedule="".join(schedule)
     schedule= stringSchedule.split("\n")
     schedule = [i for i in schedule if i]
+    schedule = grabEveryN_fromList(schedule,5)
+    
+
     return schedule
 
 def scrape_general_info():
-    soup_wiki = file2Soup2("C:\\Users\\Yujie\\OneDrive\\Desktop\\Computer Science\\COSC 4P02\\BrockChatbot\\backend\\canadaGamesWiki.htm")
+    soup_wiki = file2Soup_utf("C:\\Users\\Yujie\\OneDrive\\Desktop\\Computer Science\\COSC 4P02\\BrockChatbot\\backend\\canadaGamesWiki.htm")
     wiki_info_box = soup_wiki.find_all('p')
     wiki_info = [i.get_text() for i in wiki_info_box]
     #First 2 indices of info holds the most important information
     #print(wiki_info[0] + wiki_info[1])
 
-    soup_FAQ = file2Soup2("C:\\Users\\Yujie\\OneDrive\\Desktop\\Computer Science\\COSC 4P02\\BrockChatbot\\backend\\canadaGamesFAQ.htm")
+    soup_FAQ = file2Soup_utf("C:\\Users\\Yujie\\OneDrive\\Desktop\\Computer Science\\COSC 4P02\\BrockChatbot\\backend\\canadaGamesFAQ.htm")
     cg_containers = soup_FAQ.find_all('div', class_=('cg-container'))
     FAQ = []
     for cg_container in cg_containers:
@@ -89,9 +82,12 @@ def scrape_general_info():
     return general_info
 
 if __name__ == "__main__":
-    scrape_general_info()
+    ## Example use cases:
+    
+    #scrape_general_info()
+
     #athlete_table = scrapeAthleteData_gemspro()
-    #print(len(athlete_table))
+    #print(athlete_table)
 
     #compeititors = scrapeCompetitors_nigaragames()
     #print(len(compeititors))
