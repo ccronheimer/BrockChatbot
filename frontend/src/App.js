583647814472c122d7b814962407ca4a0492239f
Cameron <cameroncronheimer@gmail.com>
2022-02-05 23:55:52 -0500

    container

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import "./styles.css"
 
 function App() {
   return (
-    <div>
-    Test 
-    </div>
+    
+        <div className='chat-container'>
+          <div className='chat'>
+            Hello
+          </div>
+          <div className='chat-input'>
+            Hello
+          </div>
+        </div>
+ 
   );
 }
 
 export default App;
+
